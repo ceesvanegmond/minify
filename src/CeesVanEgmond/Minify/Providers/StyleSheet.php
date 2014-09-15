b3d379318eb6d9780b4461ebf60f16ea5664fa23
Antal Levente <antal.levente@minic.ro>
2014-09-15 19:43:35 +0300

    added default values

--- src/CeesVanEgmond/Minify/Providers/StyleSheet.php
+++ src/CeesVanEgmond/Minify/Providers/StyleSheet.php
@@ -25,7 +25,7 @@ class StyleSheet extends BaseProvider implements MinifyInterface
      * @param array $attributes
      * @return string
      */
-    public function tag($file, array $attributes = array(), $async, $defer)
+    public function tag($file, array $attributes = array(), $async = false, $defer = false)
     {
         $attributes['href'] = $file;
         $attributes['rel'] = 'stylesheet';
